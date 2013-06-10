d83ba4c9172a9bd8d76bd110cff186b0bcd0755f
dan kefford <dan_kefford@hotmail.com>
2013-06-10 17:43:16 -0400

    Finished off chapter 3, starting on 4.

---
+++ src/fpij/chapter3/ListSelectedFiles.java
@@ -0,0 +1,11 @@
+package fpij.chapter3;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dk234
+ * Date: 6/10/13
+ * Time: 3:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ListSelectedFiles {
+}
---
+++ src/fpij/chapter4/AssetUtil.java
@@ -0,0 +1,7 @@
+package fpij.chapter4;
+
+/**
+ * Created by dk234 on 6/10/13.
+ */
+public class AssetUtil {
+}
